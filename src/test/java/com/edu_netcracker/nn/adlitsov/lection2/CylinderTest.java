72366513c08959225f00c76d9ef1b44825003cf8
alex <adlitsov@gmail.com>
2018-02-21 00:19:58 +0300

    Added tests for Cylinder class (lect. 2, ex. 3).

---
+++ src/test/java/com/edu_netcracker/nn/adlitsov/lection2/CylinderTest.java
@@ -0,0 +1,23 @@
+package com.edu_netcracker.nn.adlitsov.lection2;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Random;
+
+class CylinderTest {
+
+    @RepeatedTest(10)
+    void getVolumeShouldUseCorrectFormula() {
+        Random r = new Random();
+        double radius = r.nextDouble() % 1000;
+        double height = r.nextDouble() % 1000;
+        double expectedVolume = (Math.PI * Math.pow(radius, 2)) * height;
+
+        Cylinder c = new Cylinder(radius, height);
+
+        double delta = 1e-5;
+        assertEquals(expectedVolume, c.getVolume(), delta);
+    }
+}
