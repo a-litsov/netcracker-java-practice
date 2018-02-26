92602c6cd3d968fadd7439da4c1fe5fda0dd0ad1
alex <adlitsov@gmail.com>
2018-02-26 23:28:24 +0300

    Added RectangleTest

---
+++ src/test/java/com/edu_netcracker/nn/adlitsov/homework1/RectangleTest.java
@@ -0,0 +1,40 @@
+package com.edu_netcracker.nn.adlitsov.homework1;
+
+import org.junit.jupiter.api.RepeatedTest;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RectangleTest {
+    public static final float MAX_VAL = 1000;
+
+    @RepeatedTest(10)
+    void getAreaShouldUseCorrectFormula() {
+        Random rnd = new Random();
+        float length = rnd.nextFloat() * MAX_VAL;
+        float width = rnd.nextFloat() * MAX_VAL;
+
+        Rectangle rect = new Rectangle(length, width);
+
+        float expectedArea = length * width;
+
+        double delta = 1e-5;
+        assertEquals(expectedArea, rect.getArea(), delta, rect + " should have area = " + expectedArea);
+    }
+
+    @RepeatedTest(10)
+    void getPerimeterShouldUseCorrectFormula() {
+        Random rnd = new Random();
+        float length = rnd.nextFloat() * MAX_VAL;
+        float width = rnd.nextFloat() * MAX_VAL;
+
+        Rectangle rect = new Rectangle(length, width);
+
+        float expectedPerimeter = 2 * (length + width);
+
+        double delta = 1e-5;
+        assertEquals(expectedPerimeter, rect.getPerimeter(), delta, rect + " should have perimeter = "
+                + expectedPerimeter);
+    }
+}
