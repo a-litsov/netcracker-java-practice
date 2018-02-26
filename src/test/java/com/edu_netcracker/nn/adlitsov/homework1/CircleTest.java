e126692a54685562cdaad32519b5a77e1172dbad
alex <adlitsov@gmail.com>
2018-02-27 00:02:04 +0300

    CircleTest: Added max value for radius and message in assertion.

--- src/test/java/com/edu_netcracker/nn/adlitsov/homework1/CircleTest.java
+++ src/test/java/com/edu_netcracker/nn/adlitsov/homework1/CircleTest.java
@@ -7,17 +7,18 @@ import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CircleTest {
+    public static final double MAX_VAL = 1000;
 
     @RepeatedTest(10)
     void areaShouldUseCorrectFormula() {
         Random r = new Random();
-        double radius = r.nextDouble();
-        Circle c = new Circle(radius);
+        double radius = r.nextDouble() * MAX_VAL;
+        Circle circ = new Circle(radius);
 
         double expectedArea = Math.PI * Math.pow(radius, 2);
 
         double delta = 1e-5d;
-        assertEquals(expectedArea, c.getArea(), delta);
+        assertEquals(expectedArea, circ.getArea(), delta, circ + " should have area = " + expectedArea);
     }
 
 }
