96594ced7045a3ceffe83490be3bf2096d050109
alex <adlitsov@gmail.com>
2018-02-26 22:24:51 +0300

    Removed useless test-cases from CircleTest

--- src/test/java/com/edu_netcracker/nn/adlitsov/lection2/CircleTest.java
+++ src/test/java/com/edu_netcracker/nn/adlitsov/lection2/CircleTest.java
@@ -9,58 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CircleTest {
 
-    @Test
-    void defaultConstructorDoesNotChangeRadius() {
-        Circle c = new Circle();
-
-        double expectedRadius = 1.0d;
-        assertEquals(expectedRadius, c.getRadius());
-    }
-
-    @RepeatedTest(10)
-    void constructorShouldCorrectlyInitRadius() {
-        Random r = new Random();
-        double expectedRadius = r.nextDouble();
-        Circle c = new Circle(expectedRadius);
-
-        assertEquals(expectedRadius, c.getRadius());
-    }
-
-    @RepeatedTest(10)
-    void constructorShouldCorrectlyInitRadiusAndColor() {
-        Random r = new Random();
-        double expectedRadius = r.nextDouble();
-        // just easy way to generate random string made of digits ;)
-        String expectedColor = Long.toString(r.nextLong());
-        Circle c = new Circle(expectedRadius, expectedColor);
-
-        assertEquals(expectedRadius, c.getRadius());
-        assertEquals(expectedColor, c.getColor());
-    }
-
-    @RepeatedTest(10)
-    void radiusShouldBeSetCorrectly() {
-        Random r = new Random();
-        double expectedRadius = r.nextDouble();
-
-        Circle c = new Circle();
-        c.setRadius(expectedRadius);
-
-        assertEquals(expectedRadius, c.getRadius());
-    }
-
-    @RepeatedTest(10)
-    void colorShouldBeSetCorrectly() {
-        Random r = new Random();
-        // just easy way to generate random string made of digits ;)
-        String expectedColor = Long.toString(r.nextLong());
-
-        Circle c = new Circle();
-        c.setColor(expectedColor);
-
-        assertEquals(expectedColor, c.getColor());
-    }
-
     @RepeatedTest(10)
     void areaShouldUseCorrectFormula() {
         Random r = new Random();
@@ -73,16 +21,4 @@ class CircleTest {
         assertEquals(expectedArea, c.getArea(), delta);
     }
 
-    @RepeatedTest(10)
-    void toStringShouldWorkCorrectly() {
-        Random r = new Random();
-        // just easy way to generate random string made of digits ;)
-        String color = Long.toString(r.nextLong());
-        double radius = r.nextDouble();
-
-        Circle c = new Circle(radius, color);
-        String expectedStrRepres = "Circle[radius=" + radius + ", color=" + color + "]";
-
-        assertEquals(expectedStrRepres, c.toString());
-    }
 }
