2fb11abe3efaa81888ddfab3b8c1fcf2cfe82f62
alex <adlitsov@gmail.com>
2018-02-27 16:46:17 +0300

    Added test for MyPoint class

---
+++ src/test/java/com/edu_netcracker/nn/adlitsov/homework1/MyPointTest.java
@@ -0,0 +1,26 @@
+package com.edu_netcracker.nn.adlitsov.homework1;
+
+import org.junit.jupiter.api.RepeatedTest;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyPointTest {
+
+    public static final int MAX_COORD = 1000;
+
+    @RepeatedTest(10)
+    void distanceShouldUseProperFormula() {
+        Random rnd = new Random();
+        int x = rnd.nextInt(MAX_COORD);
+        int y = rnd.nextInt(MAX_COORD);
+
+        MyPoint p = new MyPoint(rnd.nextInt(MAX_COORD), rnd.nextInt(MAX_COORD));
+        double distance = p.distance(x, y);
+
+        double expectedDistance = Math.sqrt(Math.pow(p.getX() - x, 2) + Math.pow(p.getY() - y, 2));
+
+        assertEquals(expectedDistance, distance, "distance must be = " + expectedDistance);
+    }
+}
