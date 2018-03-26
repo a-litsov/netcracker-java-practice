2f0924956337a28d0cec0b36ebb48cddb32cd7ab
alex <adlitsov@gmail.com>
2018-03-26 13:36:29 +0300

    ILinkedList: added ILinkedList interface

---
+++ src/main/java/com/netcracker/adlitsov/homework4/ILinkedList.java
@@ -0,0 +1,23 @@
+package com.netcracker.adlitsov.homework4;
+
+public interface ILinkedList<E> {
+    int size();
+
+    void add(E element);
+
+    void add(int index, E element);
+
+    E set(int index, E element);
+
+    E get(int index);
+
+    int indexOf(E element);
+
+    E remove(int index);
+
+    void clear();
+
+    E[] toArray();
+
+    String toString();
+}
